e3632e1a69105897316fadc071a80d9775e9a4f1
farhamim222 <Farha.Mim@metropolia.fi>
2026-09-20 21:52:37 +0300

    Add JaCoCo coverage report

--- src/test/java/TemperatureConverterTest.java
+++ src/test/java/TemperatureConverterTest.java
@@ -1,4 +1,33 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class TemperatureConverterTest {
-  
+
+    private final TemperatureConverter converter =
+            new TemperatureConverter();
+
+    @Test
+    void fahrenheitToCelsius() {
+        assertEquals(0.0, converter.fahrenheitToCelsius(32), 0.001);
+        assertEquals(100.0, converter.fahrenheitToCelsius(212), 0.001);
+        assertEquals(-40.0, converter.fahrenheitToCelsius(-40), 0.001);
+    }
+
+    @Test
+    void celsiusToFahrenheit() {
+        assertEquals(32.0, converter.celsiusToFahrenheit(0), 0.001);
+        assertEquals(212.0, converter.celsiusToFahrenheit(100), 0.001);
+        assertEquals(-40.0, converter.celsiusToFahrenheit(-40), 0.001);
+    }
+
+    @Test
+    void isExtremeTemperature() {
+        assertTrue(converter.isExtremeTemperature(-41));
+        assertTrue(converter.isExtremeTemperature(51));
+
+        assertFalse(converter.isExtremeTemperature(-40));
+        assertFalse(converter.isExtremeTemperature(50));
+        assertFalse(converter.isExtremeTemperature(20));
+    }
 }
